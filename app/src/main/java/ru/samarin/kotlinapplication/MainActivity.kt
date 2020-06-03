dd344f823b142f25492a4784093837c8951033b3
RaFFiaNBui <Raffian_bui@mail.ru>
2020-06-03 17:00:27 +0300

    Homework for lesson 1 Kotlin course

---
+++ app/src/main/java/ru/samarin/kotlinapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.samarin.kotlinapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_onclick.setOnClickListener {
+            Toast.makeText(this,"Hello World!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
