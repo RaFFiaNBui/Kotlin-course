66405e69da88b583d8e460c892d1bf40b3da97a2
RaFFiaNBui <Raffian_bui@mail.ru>
2020-08-22 16:52:01 +0300

    Homework for lesson 7 Kotlin course

--- app/src/main/java/ru/samarin/kotlinapp/ui/base/BaseViewModel.kt
+++ app/src/main/java/ru/samarin/kotlinapp/ui/base/BaseViewModel.kt
@@ -5,7 +5,7 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 
-open class BaseViewModel<T, S : BaseViewState<T>> : ViewModel() {
+open class BaseViewModel<T, S : BaseViewState<T>> : ViewModel(){
     open val viewStateLiveData = MutableLiveData<S>()
     open fun getViewState(): LiveData<S> = viewStateLiveData
 }
